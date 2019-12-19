ce72069db266f7f485df30a88d3a42cce546f6c5
cusmirlucian <53294551+cusmirlucian@users.noreply.github.com>
2019-12-19 14:08:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PinkRhino</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/69947885df.js" crossorigin="anonymous"></script>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"> 
+</head>
+<body>
+	<div class="nav">
+		<h1>PinkRhino/</h1>
+		<ul>
+			<a href=""><i class="fas fa-poll-h"></i></a>
+			<a href=""><i class="fas fa-sign-in-alt"></i></a>
+			<a href=""><i class="fas fa-key"></i></a>
+		</ul>
+	</div>
+	<div class="header">
+		<br><br><br><br><br><br><br><br><br><br>
+		<h1>Pink rhino</h1>
+	</div>
+	<div class="main">
+		<br>
+		<h1>Despre noi</h1>
+	</div>
+	<div class="about">
+		<p class="aboutp">Romania (/roʊˈmeɪniə/ (About this soundlisten) ro-MAY-nee-ə; Romanian: România [romɨˈni.a] (About this soundlisten)) is a country located at the crossroads of Central, Eastern, and Southeastern Europe. It has borders with the Black Sea to the southeast, Bulgaria to the south, Ukraine to the north, Hungary to the west, Serbia to the southwest, and Moldova to the east. It has a predominantly temperate-continental climate. With a total area of 238,397 square kilometers (92,046 square miles), Romania is the 12th largest country and also the 7th most populous member state of the European Union, having almost 20 million inhabitants. Its capital and largest city is Bucharest, and other major urban areas include Cluj-Napoca, Timișoara, Iași, Constanța, Craiova, Brașov, and Galați.</p> 
+		<div class="img"></div>
+		<p class="aboutp">Romania (/roʊˈmeɪniə/ (About this soundlisten) ro-MAY-nee-ə; Romanian: România [romɨˈni.a] (About this soundlisten)) is a country located at the crossroads of Central, Eastern, and Southeastern Europe. It has borders with the Black Sea to the southeast, Bulgaria to the south, Ukraine to the north, Hungary to the west, Serbia to the southwest, and Moldova to the east. It has a predominantly temperate-continental climate. With a total area of 238,397 square kilometers (92,046 square miles), Romania is the 12th largest country and also the 7th most populous member state of the European Union, having almost 20 million inhabitants. Its capital and largest city is Bucharest, and other major urban areas include Cluj-Napoca, Timișoara, Iași, Constanța, Craiova, Brașov, and Galați.</p> 
+	</div>
+	<div class="footer">
+		<h1>Pinkrhino</h1>
+		<hr>
+		<h2>Contact US</h2>
+		<div class="contact">
+			<i class="fab fa-facebook-f"></i>
+			<i class="fab fa-instagram"></i>
+			<i class="fas fa-at"></i>
+			<i class="fab fa-twitter"></i>
+		</div>
+	</div>
+</body>
+</html>
